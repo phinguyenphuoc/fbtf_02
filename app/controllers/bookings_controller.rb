bfc9b903fd4575df8920695ce004a42f479528af
Thanh Dat <tranlethanhdat1995@gmail.com>
2019-03-13 21:54:55 +0700

    booking

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,48 @@
+class BookingsController < ApplicationController
+  before_action :find_tour, only: [:new, :create]
+  before_action :check_quanlity, :load_booking, only: :create
+
+  def new
+    @booking = current_user.bookings.new
+  end
+
+  def create
+    ActiveRecord::Base.transaction do
+      @booking.save!
+      @tour.recent_quantity += @booking.quantity
+      @tour.update_attributes!(recent_quantity: @tour.recent_quantity)
+      flash[:success] = t "booking_success"
+    rescue ActiveRecord::RecordInvalid
+      flash[:danger] = t "booking_fail1"
+    end
+    redirect_to request.referrer
+  end
+
+  private
+
+  def check_quanlity
+    @count = @tour.max_quantity - @tour.recent_quantity
+    return unless @count < params[:booking][:quantity].to_i
+    flash[:danger] = t "booking_fail"
+    redirect_to request.referrer
+  end
+
+  def load_booking
+    @booking = current_user.bookings.new booking_params
+    @booking.tour_id = @tour.id
+    @booking.quantity = params[:booking][:quantity]
+    @booking.bill = @booking.quantity * @tour.price
+  end
+
+  def booking_params
+    params.require(:booking).permit :name, :phonenumber,
+      :address, :quantity, :note
+  end
+
+  def find_tour
+    @tour = Tour.find_by id: params[:tour_id]
+    return if @tour
+    flash[:danger] = t "no_data"
+    redirect_to root_path
+  end
+end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,4 +3,11 @@ class Booking < ApplicationRecord
   belongs_to :tour
 
   enum status: {waitting: 0, refuse: 1, approve: 2}
+
+  validates :name, presence: true, length: {maximum: Settings.booking.max_name}
+  validates :phonenumber, presence: true, numericality: true,
+            length: {minimum: Settings.booking.min_phone,
+                     maximum: Settings.booking.max_phone}
+  validates :address, presence: true
+  validates :quantity, presence: true
 end
