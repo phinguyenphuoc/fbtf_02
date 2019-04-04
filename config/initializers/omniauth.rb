ea40ceb6501eb9f271f4a0285fdb5d1494f46ea6
PhiNguyen <phinguyenphuoc@gmail.com>
2019-04-04 18:13:08 +0700

    Update login facebook with devise

--- config/initializers/omniauth.rb
+++
@@ -1,4 +0,0 @@
-OmniAuth.config.logger = Rails.logger
-Rails.application.config.middleware.use OmniAuth::Builder do
- provider :facebook, ENV['fb_app_id'], ENV['fb_app_secret'], {scope: "public_profile", info_fields: "email,name"}
-end
